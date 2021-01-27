b49a142667dd9fc70a6ffb0908cfbf51ba554155
ParkJinhui <itpjh@naver.com>
2021-01-27 11:32:11 +0900

    Initial Commit

---
+++ Covid/CovidApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidApp.swift
+//  Covid
+//
+//  Created by user on 2021/01/27.
+//
+
+import SwiftUI
+
+@main
+struct CovidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
